d564d3983c5a4a8d84dd0fe4bb9decfb984cd35a
jacobcrothers <jacobcrothers425@gmail.com>
2018-03-20 19:49:55 +0200

    first commit , stock spring boot 2.0.0 build

---
+++ binarbox/src/main/java/binar/box/BinarboxApplication.java
@@ -0,0 +1,12 @@
+package binar.box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinarboxApplication.class, args);
+	}
+}
---
+++ binarbox/src/test/java/binar/box/BinarboxApplicationTests.java
@@ -0,0 +1,16 @@
+package binar.box;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BinarboxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
