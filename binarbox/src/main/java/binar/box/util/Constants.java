941f0b51dc2d01a660662a60c54976b4aaad8d35
jacobcrothers <jacobcrothers425@gmail.com>
2018-03-20 22:07:24 +0200

    build.gradle - added pool connection dependency, added mariadb driver dependency DatabaseConfiguration.java - configured connection to database Constants.java - added two constants master.xml - added liquibase master xml application.yml  - added first application profile with database config

---
+++ binarbox/src/main/java/binar/box/util/Constants.java
@@ -0,0 +1,9 @@
+package binar.box.util;
+
+/**
+ * Created by Timis Nicu Alexandru on 20-Mar-18.
+ */
+public class Constants {
+    public static final String DATABASE_PASSWORD_PROPERTY = "password";
+    public static final String DATABASE_USERNAME_PROPERTY = "user";
+}
