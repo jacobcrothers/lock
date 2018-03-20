0bc1da72bac3175554b0815517e3cf3b5abf9be3
jacobcrothers <jacobcrothers425@gmail.com>
2018-03-20 22:37:17 +0200

    User.java - added user entity

--- binarbox/src/main/java/binar/box/BinarboxApplication.java
+++ binarbox/src/main/java/binar/box/BinarboxApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BinarboxApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BinarboxApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BinarboxApplication.class, args);
+    }
 }
---
+++ binarbox/src/main/java/binar/box/domain/User.java
@@ -0,0 +1,99 @@
+package binar.box.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+/**
+ * Created by Timis Nicu Alexandru on 20-Mar-18.
+ */
+@Table(name = "user")
+public class User {
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "reset_password_token")
+    private String resetPasswordToken;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "country")
+    private String Country;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getResetPasswordToken() {
+        return resetPasswordToken;
+    }
+
+    public void setResetPasswordToken(String resetPasswordToken) {
+        this.resetPasswordToken = resetPasswordToken;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+}
