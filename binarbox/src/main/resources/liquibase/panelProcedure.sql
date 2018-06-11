b64ca2b8a34c82c8774c0adde42a967cc76cfbd6
jacobcrothers <jacobcrothers425@gmail.com>
2018-06-11 21:40:45 +0300

    upgraded procedure

--- binarbox/src/main/resources/liquibase/panelProcedure.sql
+++ binarbox/src/main/resources/liquibase/panelProcedure.sql
@@ -1,10 +1,10 @@
 #
-CREATE PROCEDURE insertRandomPanelsEntity()
+CREATE PROCEDURE insertRandomPanelsEntity(a INT)
 BEGIN
 DECLARE i INT;
 SET i = 1;
 START TRANSACTION;
-WHILE i <= 200000 DO
+WHILE i <= a DO
 INSERT INTO lb.panel_entity (created_date,last_modified_date) VALUES(CURRENT_DATE,CURRENT_DATE);
 SET i = i + 1;
 END WHILE;
