5e29b67d615d64ab6c13e6aec6846faac7ecc680
jacobcrothers <jacobcrothers425@gmail.com>
2018-08-06 14:14:52 +0300

    Removed user change password, update user and other functionalities for the user logged in with email.

--- binarbox/src/main/java/binar/box/rest/UserController.java
+++ binarbox/src/main/java/binar/box/rest/UserController.java
@@ -1,23 +1,13 @@
 package binar.box.rest;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import binar.box.dto.ChangePasswordDTO;
-import binar.box.dto.ResetPasswordDTO;
 import binar.box.dto.UserProfileDTO;
 import binar.box.service.UserService;
 import binar.box.util.Constants;
-import binar.box.util.LockBridgesException;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 
@@ -31,44 +21,10 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@PostMapping(value = Constants.USER_ENDPOINT + Constants.REQUEST_RESET_PASSWORD_EDNPOINT)
-	private void requestResetPassword(@Param("email") String email) {
-		userService.requestResetPassword(email);
-	}
-
-	@PostMapping(value = Constants.USER_ENDPOINT + Constants.RESET_PASSWORD_ENDPOINT)
-	private void resetPassword(@Param("token") String token, @RequestBody @Valid ResetPasswordDTO resetPasswordDTO,
-			BindingResult bindingResult) {
-		if (bindingResult.hasErrors()) {
-			throw new LockBridgesException(bindingResult.getAllErrors().toString());
-		}
-		userService.changeUserPassword(token, resetPasswordDTO);
-	}
-
-	@PostMapping(value = Constants.USER_ENDPOINT + Constants.CONFIRM_EMAIL_ENDPOINT)
-	private void confirmEmail(@Param("token") String token) {
-		userService.confirmUserEmail(token);
-	}
-
 	@ApiImplicitParams({
 			@ApiImplicitParam(name = "token", value = "ex: eyJ0eXAiO....", dataType = "string", paramType = "header") })
 	@GetMapping(value = Constants.USER_ENDPOINT)
 	private UserProfileDTO getUser() {
 		return userService.getUser();
 	}
-
-	@ApiImplicitParams({
-			@ApiImplicitParam(name = "token", value = "ex: eyJ0eXAiO....", dataType = "string", paramType = "header") })
-	@PutMapping(value = Constants.USER_ENDPOINT)
-	private void updateUser(@RequestBody UserProfileDTO userProfileDTO) {
-		userService.updateUser(userProfileDTO);
-	}
-
-	@ApiImplicitParams({
-			@ApiImplicitParam(name = "token", value = "ex: eyJ0eXAiO....", dataType = "string", paramType = "header") })
-	@PostMapping(value = Constants.USER_ENDPOINT + Constants.CHANGE_PASSWORD)
-	private void changeUserPassword(@RequestBody ChangePasswordDTO changePasswordDTO) {
-		userService.changeUserPassword(changePasswordDTO);
-	}
-
 }
--- binarbox/src/main/java/binar/box/service/UserService.java
+++ binarbox/src/main/java/binar/box/service/UserService.java
@@ -3,9 +3,6 @@ package binar.box.service;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import javax.transaction.Transactional;
 
@@ -13,20 +10,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.social.facebook.api.impl.FacebookTemplate;
-import org.springframework.social.support.URIBuilder;
 import org.springframework.stereotype.Service;
 
 import binar.box.domain.Lock;
 import binar.box.domain.User;
-import binar.box.dto.ChangePasswordDTO;
 import binar.box.dto.FacebookTokenDTO;
-import binar.box.dto.ResetPasswordDTO;
 import binar.box.dto.UserProfileDTO;
 import binar.box.repository.LockRepository;
 import binar.box.repository.UserRepository;
@@ -46,52 +36,12 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 
-	@Autowired
-	private EmailService emailService;
-
-	@Autowired
-	private Environment environment;
-
-	private ExecutorService executors = Executors.newFixedThreadPool(2);
-
 	private User getUserByEmail(String email) {
 		return userRepository.findByEmail(email).orElseThrow(() -> new LockBridgesException(Constants.USER_NOT_FOUND));
 	}
 
-	public void requestResetPassword(String email) {
-		User user = getUserByEmail(email);
-		String resetPasswordToken = UUID.randomUUID().toString();
-		user.setResetPasswordToken(resetPasswordToken);
-		emailService.sendEmail(user.getEmail(), "Reset Password", resetPasswordToken);
-		userRepository.save(user);
-	}
-
-	public void changeUserPassword(String token, ResetPasswordDTO resetPasswordDTO) {
-		User user = getUserByResetPasswordToken(token);
-		user.setPassword(BCrypt.hashpw(resetPasswordDTO.getPassword(), BCrypt.gensalt(12)));
-		user.setResetPasswordToken(null);
-		userRepository.save(user);
-	}
-
-	private User getUserByResetPasswordToken(String token) {
-		return userRepository.findByResetPasswordToken(token)
-				.orElseThrow(() -> new LockBridgesException(Constants.USER_NOT_FOUND));
-	}
-
 	User getAuthenticatedUser() {
 		return getUserByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
-	}
-
-	public void confirmUserEmail(String token) {
-		var user = getUserByEmailToken(token);
-		user.setConfirmEmailToken(null);
-		user.setEmailConfirmed(true);
-		userRepository.save(user);
-	}
-
-	private User getUserByEmailToken(String token) {
-		return userRepository.findByConfirmEmailToken(token)
-				.orElseThrow(() -> new LockBridgesException(Constants.USER_NOT_FOUND));
 	}
 
 	public void loginUser(FacebookTokenDTO facebookTokenDTO) {
@@ -104,7 +54,6 @@ public class UserService {
 		var user = userRepository.findByFacebookIdOrEmail(facebookUser.getId(), facebookUser.getEmail());
 		if (user.isPresent()) {
 			var registeredUser = user.get();
-			executors.submit(() -> getLongLiveFacebookToken(registeredUser, accessToken));
 			registeredUser.setEmailConfirmed(true);
 			registeredUser.setLinkedWithFacebbok(true);
 			userRepository.save(registeredUser);
@@ -112,24 +61,6 @@ public class UserService {
 		var toRegisterUser = new User();
 		facebookUserToOurUser(facebookUser, toRegisterUser, accessToken);
 		userRepository.save(toRegisterUser);
-		executors.submit(() -> getLongLiveFacebookToken(toRegisterUser, accessToken));
-	}
-
-	private void getLongLiveFacebookToken(User user, String token) {
-		var facebookTemplate = new FacebookTemplate(token);
-		var clientId = environment.getProperty(Constants.FACEBOOK_CLIENT_ID);
-		var clientSecret = environment.getProperty(Constants.FACEBOOK_CLIENT_SECRET);
-		var uri = URIBuilder.fromUri(Constants.HTTPS_GRAPH_FACEBOOK_COM_OAUTH_ACCESS_TOKEN)
-				.queryParam(Constants.GRANT_TYPE, Constants.FB_EXCHANGE_TOKEN).queryParam(Constants.CLIENT_ID, clientId)
-				.queryParam(Constants.CLIENT_SECRET, clientSecret).queryParam(Constants.FB_EXCHANGE_TOKEN, token)
-				.build();
-		var facebookLongLiveToken = facebookTemplate.restOperations().exchange(uri.toString(), HttpMethod.GET,
-				HttpEntity.EMPTY, String.class);
-		var accessToken = facebookLongLiveToken.toString().split(Constants.DOUBLE_QUOTE);
-		if (accessToken.length >= 2) {
-			user.setFacebookAccessToken(accessToken[3]);
-			userRepository.save(user);
-		}
 	}
 
 	private void facebookUserToOurUser(org.springframework.social.facebook.api.User facebookUser, User toRegisterUser,
@@ -149,25 +80,6 @@ public class UserService {
 
 	public UserProfileDTO getUser() {
 		return new UserProfileDTO(getAuthenticatedUser());
-	}
-
-	public void updateUser(UserProfileDTO userProfileDTO) {
-		var user = getAuthenticatedUser();
-		user.setLastName(userProfileDTO.getLastName());
-		user.setFirstName(userProfileDTO.getFirstName());
-		user.setPhone(userProfileDTO.getPhone());
-		user.setCity(userProfileDTO.getCity());
-		user.setCountry(userProfileDTO.getCountry());
-		user.setAddress(userProfileDTO.getAddress());
-		userRepository.save(user);
-	}
-
-	public void changeUserPassword(ChangePasswordDTO changePasswordDTO) {
-		var user = getAuthenticatedUser();
-		if (!BCrypt.checkpw(changePasswordDTO.getOldPassword(), user.getPassword())) {
-			throw new LockBridgesException(Constants.OLD_PASSWORD_DOES_NOT_MATCH);
-		}
-		user.setPassword(BCrypt.hashpw(changePasswordDTO.getPassword(), BCrypt.gensalt(12)));
 	}
 
 	public List<Lock> getUserFriendsLocks(User user) {
