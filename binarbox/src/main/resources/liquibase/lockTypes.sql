a6f5f33397bd85feca3757d8c412981b89756947
jacobcrothers <jacobcrothers425@gmail.com>
2018-08-06 19:35:39 +0300

    Added default types and templates

--- binarbox/src/main/resources/liquibase/lockTypes.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `lb`.`lock_type` (`id`, `lock_type`, `price`) VALUES ('1', 'REGULAR', '100');
-INSERT INTO `lb`.`lock_type` (`id`, `lock_type`, `price`) VALUES ('2', 'VIP', '1000');
-INSERT INTO `lb`.`lock_type` (`id`, `lock_type`, `price`) VALUES ('3', 'PREMIUM', '10000');
