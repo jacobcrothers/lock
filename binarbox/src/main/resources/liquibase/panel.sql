df2309d95bf94a905f82502331ad5f8022c1642e
jacobcrothers <jacobcrothers425@gmail.com>
2018-08-23 17:22:18 +0300

    added default panel and changed lockType and lockTypeTemaplte  default file name

---
+++ binarbox/src/main/resources/liquibase/panel.sql
@@ -0,0 +1,16 @@
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
+INSERT INTO `lb`.`panel_entity` (`created_date`, `last_modified_date`) VALUES ('2018-08-23', '2018-08-23 17:19:20');
