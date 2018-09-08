c49b14c7e3724b3241cfbc19fcc14ccd1a307646
jacobcrothers <jacobcrothers425@gmail.com>
2018-09-08 15:44:51 +0300

    -small fix

--- binarbox/src/front/src/app/user/register/register.component.ts
+++ binarbox/src/front/src/app/user/register/register.component.ts
@@ -16,6 +16,6 @@ export class RegisterComponent implements OnInit {
     }
 
     public register(formValue) {
-        this.user.register(formValue);
+        // this.user.register(formValue);
     }
 }
