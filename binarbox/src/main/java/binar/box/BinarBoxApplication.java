e531739155ba3131ec0a39748f4c1ab6e6f0f3a6
jacobcrothers <jacobcrothers425@gmail.com>
2018-12-24 23:28:36 +0200

    LOCK-12 - added get glitter locks query for see all locks LOCK-0 - simplified app bootstrap - added config for tests

--- binarbox/src/main/java/binar/box/BinarBoxApplication.java
+++ binarbox/src/main/java/binar/box/BinarBoxApplication.java
@@ -4,9 +4,16 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BinarBoxApplication {
+public class BinarBoxApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BinarBoxApplication.class);
+	}
 
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
---
+++ binarbox/src/test/java/binar/box/config/AppConfig.java
@@ -0,0 +1,9 @@
+package binar.box.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan( basePackages = {"binar.box"})
+public class AppConfig {
+}
