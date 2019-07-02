f1a0abb1190705a01ee730a7dabef8d434b5409b
jacobcrothers <jacobcrothers425@gmail.com>
2019-07-02 21:12:31 +0300

    video changes video example

---
+++ binarbox/src/main/java/binar/box/util/ModifyMediaExample.java
@@ -0,0 +1,114 @@
+package binar.box.util;
+
+import com.xuggle.mediatool.*;
+import com.xuggle.mediatool.event.IAudioSamplesEvent;
+import com.xuggle.mediatool.event.IVideoPictureEvent;
+import com.xuggle.xuggler.IAudioResampler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ShortBuffer;
+
+public class ModifyMediaExample {
+
+    private static final String inputFilename = "C:\\Users\\Andrei\\Desktop\\result.mp4";
+    private static final String outputFilename = "C:\\Users\\Andrei\\Desktop\\testResultNew.mp4";
+    private static final String imageFilename = "C:\\Users\\Andrei\\Desktop\\lacatTest.png";
+
+    private IAudioResampler audioResampler = null;
+
+    public static void main(String[] args) {
+
+        // create a media reader
+        IMediaReader mediaReader = ToolFactory.makeReader(inputFilename);
+
+        // configure it to generate BufferImages
+        mediaReader.setBufferedImageTypeToGenerate(BufferedImage.TYPE_3BYTE_BGR);
+
+        IMediaWriter mediaWriter =
+                ToolFactory.makeWriter(outputFilename, mediaReader);
+
+        IMediaTool imageMediaTool = new StaticImageMediaTool(imageFilename);
+        IMediaTool audioVolumeMediaTool = new VolumeAdjustMediaTool(0.1);
+
+        // create a tool chain:
+        // reader -> addStaticImage -> reduceVolume -> writer
+        mediaReader.addListener(imageMediaTool);
+        imageMediaTool.addListener(audioVolumeMediaTool);
+        audioVolumeMediaTool.addListener(mediaWriter);
+
+        while (mediaReader.readPacket() == null) ;
+
+    }
+
+    private static class StaticImageMediaTool extends MediaToolAdapter {
+
+        private BufferedImage logoImage;
+
+//        AtomicInteger frameNumber= new AtomicInteger(0);
+        int frameNumber= 0;
+
+        public StaticImageMediaTool(String imageFile) {
+
+            try {
+                logoImage = ImageIO.read(new File(imageFile));
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Could not open file");
+            }
+
+        }
+
+        @Override
+        public void onVideoPicture(IVideoPictureEvent event) {
+//            frameNumber.getAndIncrement();
+            frameNumber++;
+//            System.out.println(frameNumber.get());
+            System.out.println(frameNumber);
+            BufferedImage image = event.getImage();
+
+//            if (frameNumber.get()>2755) {
+            if (frameNumber>240) {
+                // get the graphics for the image
+                ImageUtils.addTextToBufferedImage(
+                        image,
+                        "Love U",
+                        new Font("Candara", Font.BOLD, 45),
+                        Color.BLUE);
+            }
+
+            // call parent which will pass the video onto next tool in chain
+            super.onVideoPicture(event);
+
+        }
+
+    }
+
+    private static class VolumeAdjustMediaTool extends MediaToolAdapter {
+
+        // the amount to adjust the volume by
+        private double mVolume;
+
+        public VolumeAdjustMediaTool(double volume) {
+            mVolume = volume;
+        }
+
+        @Override
+        public void onAudioSamples(IAudioSamplesEvent event) {
+
+            // get the raw audio bytes and adjust it's value
+            ShortBuffer buffer =
+                    event.getAudioSamples().getByteBuffer().asShortBuffer();
+            for (int i = 0; i < buffer.limit(); ++i) {
+                buffer.put(i, (short) (buffer.get(i) * mVolume));
+            }
+
+            // call parent which will pass the audio onto next tool in chain
+            super.onAudioSamples(event);
+        }
+    }
+}
