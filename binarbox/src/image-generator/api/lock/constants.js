d42f773835379991dc2fd761d7a09aa427b61593
jacobcrothers <jacobcrothers425@gmail.com>
2019-10-03 19:51:06 +0300

    Change images dim

--- binarbox/src/image-generator/api/lock/constants.js
+++ binarbox/src/image-generator/api/lock/constants.js
@@ -1,11 +1,17 @@
 //TODO update dimensions
-const IMAGE = {
-  WIDTH: 400,
+const IMAGE_WITH_TEXT = {
+  WIDTH: 420,
   HEIGHT: 300,
 };
 
+const IMAGE_WITHOUT_TEXT = {
+  WIDTH: 1920,
+  HEIGHT: 1080,
+};
+
 const TEXT = {
-  LINE_HEIGHT: 15
+  LINE_HEIGHT: 15,
+  LINE_HEIGHT_WITHOUT_IMAGE: 52
 };
 
 const MIME_TYPES = {
@@ -16,21 +22,21 @@ const MIME_TYPES = {
 const LINE_END = '{LINE_END}';
 
 const HTTP_HEADERS = {
-    CONTENT_TYPE: "Content-Type"
+  CONTENT_TYPE: 'Content-Type',
 };
 
-const API_URL = "https://localhost:6060/api/v0/download/file/{LOCK_ID}";
-
+const API_URL = 'https://localhost:6060/api/v0/download/file/{LOCK_ID}';
 
 const FONT = {
-    ARIAL: "Arial"
+  ARIAL: 'Arial',
 };
 
 module.exports = {
   MIME_TYPES: MIME_TYPES,
   LINE_END: LINE_END,
-  IMAGE: IMAGE,
+  IMAGE_WITH_TEXT: IMAGE_WITH_TEXT,
+  IMAGE_WITHOUT_TEXT: IMAGE_WITHOUT_TEXT,
   HTTP_HEADERS: HTTP_HEADERS,
   API_URL: API_URL,
-  TEXT: TEXT
+  TEXT: TEXT,
 };
