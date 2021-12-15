8c9fc5fe698d63817577c5734e17665651ab7be5
jacobcrothers <jacobcrothers425@gmail.com>
2021-12-15 02:55:05 -0500

    FE: update auto/manual scroll

--- binarbox/src/front/src/app/bridge/panels/panels.component.ts
+++ binarbox/src/front/src/app/bridge/panels/panels.component.ts
@@ -80,6 +80,7 @@ export class PanelsComponent implements OnInit, AfterViewInit {
         document.addEventListener('wheel', (e) => {
             this.onMousewheel(e);
         });
+        this.onMousewheel1();
     }
 
     ngAfterViewInit() {
@@ -93,7 +94,22 @@ export class PanelsComponent implements OnInit, AfterViewInit {
             tap(this.onMousewheel)
         );
     }
-
+    //for testing automatic scrolling
+    public onMousewheel1() {
+        if (this.zoomCount < 3) {
+            setTimeout(() => {
+                this.zoomCount = 1;
+                setTimeout(() => {
+                    this.zoomCount = 2;
+                    setTimeout(() => {
+                        this.zoomCount = 3;
+                        this.enableMapHighlight();
+                        // TODO: refactor
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }
+    }
     /**
      * @param firstSectionId first section id
      * @param sectionOptions
