5e940b0a94289bd8f441ca0af2e6b665ca27c51b
jacobcrothers <jacobcrothers425@gmail.com>
2021-12-20 16:53:29 -0500

    Fixed lock-panels resolution issue

--- binarbox/src/front/src/app/bridge/panels/panels.component.ts
+++ binarbox/src/front/src/app/bridge/panels/panels.component.ts
@@ -31,6 +31,9 @@ export class PanelsComponent implements OnInit, AfterViewInit, OnDestroy {
     public zoomCount = 0;
     public screenHeight: any;
     public imageWidth: number;
+    public imageleft: any;
+    public imagestick: any;
+    public smallwidth: any;
     public panelSections: ReturnType<BridgeSection['getFlattenObject']>[];
     public panelSections1: any = [];
     public panelSections2: any = [];
@@ -56,12 +59,21 @@ export class PanelsComponent implements OnInit, AfterViewInit, OnDestroy {
     }
 
     ngOnInit() {
+        // image ratio times viewport height -> so that the image won't be stretched
         if (this.addLockService.getLockId() === undefined) {
             this.router.navigate(['/locks/add-lock']);
         }
 
         this.createdLock = this.addLockService.createdLock;
         this.imageWidth = 6.25 * window.innerHeight;
+
+        this.imageleft = this.imageWidth * 0.01216;
+        this.imageleft = this.imageleft.toString() + "px";
+
+        this.imagestick = this.imageWidth * 0.006548 / 2;
+        this.imagestick = this.imagestick.toString() + "px";
+
+        this.smallwidth = this.imageWidth * 0.0625;
 
         const firstSectionsRowHeight = 189;
         const bridgePipeWidth = 29;
@@ -234,6 +246,13 @@ export class PanelsComponent implements OnInit, AfterViewInit, OnDestroy {
         this.screenHeight = event.target.innerHeight;
         // image ratio times viewport height -> so that the image won't be stretched
         this.imageWidth = 6.25 * this.screenHeight;
+        this.imageleft = this.imageWidth * 0.01216;
+        this.imageleft = this.imageleft.toString() + "px";
+
+        this.imagestick = this.imageWidth * 0.006548 / 2;
+        this.imagestick = this.imagestick.toString() + "px";
+
+        this.smallwidth = this.imageWidth * 0.0625;
     }
 
     /**
@@ -263,7 +282,6 @@ export class PanelsComponent implements OnInit, AfterViewInit, OnDestroy {
     public chooseSection(event) {
         event.preventDefault();
         // send this to BE
-         console.log("chooseSection");
         this.selectedSection = Number(event.target.id.split('-')[1]);
         this.addLockService.savePanelSection(this.selectedSection).subscribe(data => {
             this.router.navigate([`dashboard/payment`]);
@@ -289,6 +307,5 @@ export class PanelsComponent implements OnInit, AfterViewInit, OnDestroy {
 
     public pageScroll() {
         this.autoScroll();
-        console.log(this.panelSections, "++++");
     }
 }
